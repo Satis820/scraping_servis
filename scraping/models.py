0d046d23baa76e5dd7763b71840665c922eac887
Your Name <polzadp@gmail.com>
2020-10-07 16:39:26 +0300

    commit 3

--- scraping/models.py
+++ scraping/models.py
@@ -1,14 +1,37 @@
 from django.db import models
 
+from scraping.utils import from_cyrillic_to_eng
+
 
 class City(models.Model):
-    name = models.CharField(max_length=50,
-                            verbose_name='Название населенного пункта')
-    slug = models.CharField(max_length=50, blank=True)
+    name = models.CharField(max_length=50, verbose_name='Название населенного пункта', unique=True)
+    slug = models.CharField(max_length=50, blank=True, unique=True)
 
     class Meta:
-        verbose_name='Название населенного пункта'
+        verbose_name = 'Название населенного пункта'
         verbose_name_plural = 'Названия населенных пунктов'
 
     def __str__(self):
         return self.name
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = from_cyrillic_to_eng(str(self.name))
+        super().save(*args, **kwargs)
+
+
+class Language(models.Model):
+    name = models.CharField(max_length=50, verbose_name='Язык програмирования', unique=True)
+    slug = models.CharField(max_length=50, blank=True, unique=True)
+
+    class Meta:
+        verbose_name = 'Язык програмирования'
+        verbose_name_plural = 'Языки програмирования'
+
+    def __str__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = from_cyrillic_to_eng(str(self.name))
+        super().save(*args, **kwargs)
